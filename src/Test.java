145dd22710b96dfa8f81e07c3237f55f0dc25a7f
unknown <meherharshal924@gmail.com>
2023-06-05 19:46:48 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("      * * *           *  * *");
+		System.out.println("    *       *       *        *");
+		System.out.println("  *           *   *            *");
+		System.out.println(" *              *               *");
+		System.out.println("*                                *");
+		System.out.println("*      WELCOME TO MY PROJECT     *");
+		System.out.println(" *                               *");
+		System.out.println("  *                             *");
+		System.out.println("    *                         *");
+		System.out.println("      *                     *");
+		System.out.println("        *                 *");
+		System.out.println("           *           *");
+		System.out.println("              *     *");
+		System.out.println("                 *");
+
+	}
+
+}
